365795975261bbf3471eeeae8ac8d0501eb85a9e
YeEeck <yechenkai@foxmail.com>
2023-01-17 20:15:00 +0800

    修改总览页面BUG与样式修改

--- src/components/EditView/TotalInformationCard/EchartsPieChartPriceComposition.vue
+++ src/components/EditView/TotalInformationCard/EchartsPieChartPriceComposition.vue
@@ -1,8 +1,8 @@
 <template>
-    <div
-        id="EchartsPieChartPriceComposition"
-        style="width: 500px; height: 500px"
-    ></div>
+  <div
+    id="EchartsPieChartPriceComposition"
+    style="width: 500px; height: 500px"
+  ></div>
 </template>
 
 <script>
@@ -12,12 +12,12 @@ import * as echarts from "echarts/core"
 import { PieChart } from "echarts/charts"
 // 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
 import {
-    TitleComponent,
-    TooltipComponent,
-    GridComponent,
-    DatasetComponent,
-    TransformComponent,
-    GraphicComponent,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  GraphicComponent,
 } from "echarts/components"
 // 标签自动布局、全局过渡动画等特性
 import { LabelLayout, UniversalTransition } from "echarts/features"
@@ -26,105 +26,105 @@ import { SVGRenderer } from "echarts/renderers"
 
 // 注册必须的组件
 echarts.use([
-    TitleComponent,
-    TooltipComponent,
-    GridComponent,
-    DatasetComponent,
-    TransformComponent,
-    GraphicComponent,
-    PieChart,
-    LabelLayout,
-    UniversalTransition,
-    SVGRenderer,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  GraphicComponent,
+  PieChart,
+  LabelLayout,
+  UniversalTransition,
+  SVGRenderer,
 ])
 let myChart = null
 export default {
-    data: () => ({
-        price_data_processed: [],
-        total_money: 0,
-    }),
-    mounted() {
-        myChart = echarts.init(
-            document.getElementById("EchartsPieChartPriceComposition")
-        )
-    },
-    watch: {
-        "$store.state.inner_component_data": {
-            deep: true,
-            handler: function (newValue) {
-                // 将价格数据处理为Echarts能够接受的形式
-                let res = []
-                for (let index = 0; index < newValue.length; index++) {
-                    const element = newValue[index]
-                    res.push({
-                        value: element.cost * element.amount,
-                        name: element.type,
-                    })
-                    this.price_data_processed = res
-                }
-                let total_money = 0
-                for (let index = 0; index < newValue.length; index++) {
-                    const element = newValue[index]
-                    total_money += element.cost * element.amount
-                }
-                this.total_money = total_money
-                myChart.setOption({
-                    series: [
-                        {
-                            name: "价格组成",
-                            type: "pie",
-                            data: this.price_data_processed,
-                            radius: ["30%", "60%"],
-                            label: {
-                                //饼图文字的显示
-                                show: true, //默认  显示文字
-                                formatter: function (arg) {
-                                    return arg.name + "\n" + arg.percent + "%"
-                                },
-                            },
-                        },
-                    ],
-                    graphic: [
-                        {
-                            type: "circle",
-                            id: "circle",
-                            left: 178,
-                            top: 178,
-                            shape: {
-                                r: 72,
-                            },
-                            style: {
-                                fill: "#00ACC1",
-                            },
-                        },
-                        {
-                            type: "text",
-                            left: "center",
-                            top: "45%",
-                            style: {
-                                text: this.total_money,
-                                fill: "#fff",
-                                width: 30,
-                                height: 30,
-                                fontSize: 18,
-                            },
-                        },
-                        {
-                            type: "text",
-                            left: "center",
-                            top: "55%",
-                            style: {
-                                text: "总价",
-                                fill: "#fff",
-                                width: 30,
-                                height: 30,
-                                fontSize: 20,
-                            },
-                        },
-                    ],
-                })
+  data: () => ({
+    price_data_processed: [],
+    total_money: 0,
+  }),
+  mounted() {
+    myChart = echarts.init(
+      document.getElementById("EchartsPieChartPriceComposition")
+    )
+  },
+  watch: {
+    "$store.state.inner_component_data": {
+      deep: true,
+      handler: function (newValue) {
+        // 将价格数据处理为Echarts能够接受的形式
+        let res = []
+        for (let index = 0; index < newValue.length; index++) {
+          const element = newValue[index]
+          res.push({
+            value: element.cost * element.amount,
+            name: element.type,
+          })
+          this.price_data_processed = res
+        }
+        let total_money = 0
+        for (let index = 0; index < newValue.length; index++) {
+          const element = newValue[index]
+          total_money += element.cost * element.amount
+        }
+        this.total_money = total_money
+        myChart.setOption({
+          series: [
+            {
+              name: "价格组成",
+              type: "pie",
+              data: this.price_data_processed,
+              radius: ["30%", "60%"],
+              label: {
+                //饼图文字的显示
+                show: true, //默认  显示文字
+                formatter: function (arg) {
+                  return arg.name + "\n" + arg.percent + "%"
+                },
+              },
             },
-        },
+          ],
+          graphic: [
+            {
+              type: "circle",
+              id: "circle",
+              left: 178,
+              top: 178,
+              shape: {
+                r: 72,
+              },
+              style: {
+                fill: "#00ACC1",
+              },
+            },
+            {
+              type: "text",
+              left: "center",
+              top: "45%",
+              style: {
+                text: this.total_money + " 元",
+                fill: "#fff",
+                width: 30,
+                height: 30,
+                fontSize: 18,
+              },
+            },
+            {
+              type: "text",
+              left: "center",
+              top: "52%",
+              style: {
+                text: "价格组成",
+                fill: "#fff",
+                width: 30,
+                height: 30,
+                fontSize: 22,
+              },
+            },
+          ],
+        })
+      },
     },
+  },
 }
 </script>
