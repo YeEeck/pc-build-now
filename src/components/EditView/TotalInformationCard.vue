2a105b3ef467e854425a2726b2d135864f57689c
YeEeck <yechenkai@foxmail.com>
2023-01-17 21:10:06 +0800

    完成提示框集合组件样例

--- src/components/EditView/TotalInformationCard.vue
+++ src/components/EditView/TotalInformationCard.vue
@@ -1,34 +1,39 @@
 <template>
+  <div>
+    <alert-collection></alert-collection>
     <div class="wrap">
-        <div class="data-outer-block ma-2" style="flex: 1">
-            <information-collection></information-collection>
-        </div>
-        <div class="data-outer-block ma-2">
-            <echarts-pie-chart-price-composition></echarts-pie-chart-price-composition>
-        </div>
+      <div class="data-outer-block ma-2" style="flex: 1">
+        <information-collection></information-collection>
+      </div>
+      <div class="data-outer-block ma-2">
+        <echarts-pie-chart-price-composition></echarts-pie-chart-price-composition>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import EchartsPieChartPriceComposition from "./TotalInformationCard/EchartsPieChartPriceComposition.vue"
 import InformationCollection from "./TotalInformationCard/InformationCollection.vue"
+import AlertCollection from "./TotalInformationCard/AlertCollection.vue"
 export default {
-    components: {
-        EchartsPieChartPriceComposition,
-        InformationCollection,
-    },
+  components: {
+    EchartsPieChartPriceComposition,
+    InformationCollection,
+    AlertCollection,
+  },
 }
 </script>
 
 <style scoped>
 .wrap {
-    display: flex;
+  display: flex;
 }
 
 .data-outer-block {
-    background: rgb(248, 248, 248);
-    border-radius: 10px;
+  background: rgb(248, 248, 248);
+  border-radius: 10px;
 
-    width: fit-content;
+  width: fit-content;
 }
 </style>
--- src/components/EditView/TotalInformationCard/AlertCollection.vue
+++ src/components/EditView/TotalInformationCard/AlertCollection.vue
@@ -1,7 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <v-alert type="warning" dismissible>
+      主机电源尚未配置，请核查。如果你确定配置单中不包含上述配件，则可以点击右侧按钮关闭。
+
+      <!-- <v-col class="shrink">
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn outlined v-bind="attrs" v-on="on">我已核查无误</v-btn>
+            </template>
+            <span>如果你确定配置单中不包含上述配件，则点击确认</span>
+          </v-tooltip>
+        </v-col>
+      </v-row> -->
+    </v-alert>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+.data-outer-block {
+  background: rgb(248, 248, 248);
+  border-radius: 10px;
+
+  width: fit-content;
+}
+</style>
