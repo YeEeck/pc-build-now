d7c022c45e06324e805ddbca810bf7cf6a8388b3
YeEeck <yechenkai@foxmail.com>
2023-01-17 22:30:33 +0800

    信息汇总页面增加配件缺失提示

--- src/components/EditView/TotalInformationCard/AlertCollection.vue
+++ src/components/EditView/TotalInformationCard/AlertCollection.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <v-alert type="warning" dismissible>
-      主机电源尚未配置，请核查。如果你确定配置单中不包含上述配件，则可以点击右侧按钮关闭。
+      主机
+      {{ neededComponentList2String }}
+      尚未配置，请核查。如果你确定配置单中不包含上述配件，则可以点击右侧按钮关闭该提示。
 
       <!-- <v-col class="shrink">
           <v-tooltip top>
@@ -17,7 +19,71 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    needed_component_list: [],
+  }),
+  computed: {
+    neededComponentList2String() {
+      let res = this.needed_component_list[0]
+      for (let index = 1; index < this.needed_component_list.length; index++) {
+        const element = this.needed_component_list[index]
+        res += "、" + element
+      }
+      return res
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.getInnerComponentUnseted()
+    })
+  },
+  watch: {
+    "$store.state.inner_component_data": {
+      deep: true,
+      handler: function () {
+        this.getInnerComponentUnseted()
+      },
+    },
+  },
+  methods: {
+    getInnerComponentUnseted() {
+      this.needed_component_list = [
+        "CPU",
+        "主板",
+        "固态硬盘",
+        "机械硬盘",
+        "内存",
+        "电源",
+        "CPU散热器",
+        "显卡",
+        "机箱",
+      ]
+      for (
+        let index = 0;
+        index < this.$store.state.inner_component_data.length;
+        index++
+      ) {
+        const element = this.$store.state.inner_component_data[index]
+        if (element.type == "CPU主板套装") {
+          this.needed_component_list.splice(
+            this.needed_component_list.indexOf("CPU"),
+            1
+          )
+          this.needed_component_list.splice(
+            this.needed_component_list.indexOf("主板"),
+            1
+          )
+          continue
+        }
+        let pos = this.needed_component_list.indexOf(element.type)
+        if (pos != -1) {
+          this.needed_component_list.splice(pos, 1)
+        }
+      }
+    },
+  },
+}
 </script>
 
 <style scoped>
